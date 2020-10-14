a38f6fcce3f27b56b20fd4e95342a85892a3d555
Danhua Cai <danhuacai123@gmail.com>
2020-10-14 15:58:57 -0400

    new version of scope_parser and validate.py

--- scope_parser/scope_parser.py
+++ scope_parser/scope_parser.py
@@ -1,108 +1,124 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
+import sys
 
 
-# In[2]:
+def insert_UniqueID(df):
+    """
+    insert a new column of Unique ID from 1,2,3,4,5,...to n
+    :param df: a dataframe
+    :rtype: None
+    """
+    df.insert(0, "UniqueID", range(1, len(df) + 1), True)
 
 
-# read the csv "test_demo.csv" can be replace with
-# any other filename you want to input
-df = pd.read_csv("test_demo.csv")
+def expand_Twitter_Handle(df):
+    """
+    expand the records for all the associated twitter handle
+    :param df: a dataframe
+    :rtype: dataframe
+    """
 
-# create a new column named "UniqueID" from
-# 1,2,3,4,5,...to n(n is # of rows we have)
-# UniqueID is using to link the Source and Associate Twitter Handle,
-# which should create by system
-# usually it is used by system only
-df.insert(0, "UniqueID", range(1, len(df) + 1), True)
+    # to find every non-empty Associate Twitter Handle's rows's index
+    # and store into not_null_twitter
+    # for example if row 0 exist associate twitter handle, then it will
+    # be stored in not_null_twitter
+    not_null_twitter = df.loc[pd.notna(df["Associated Twitter Handle"]), :].index  # nopep8
 
-# showing the csv
-df
+    # create new rows for every Associate Twitter Handle, in this case source
+    # will be the old assciated twitter handle
+    # Type to be "Twitter Handle", UniqueID, Tags and Name will keep same.
+    new_df_to_add_twitter = pd.DataFrame({'UniqueID': df.iloc[not_null_twitter, 0],  # nopep8
+                                  'Source': df.iloc[not_null_twitter, 5],  # nopep8
+                                  "RSS feed URLs (where available)": np.nan,  # nopep8
+                                  'Type': "Twitter Handle",  # nopep8
+                                  'Tags': df.iloc[not_null_twitter, 4],  # nopep8
+                                  'Associated Twitter Handle': np.nan,  # nopep8
+                                  'Associated Publisher': np.nan,  # nopep8
+                                  'Name': df.iloc[not_null_twitter, 7],  # nopep8
+                                  'Text aliases': np.nan})
 
-
-# In[3]:
-
-
-# to find every non-empty Associate Twitter Handle's rows's index
-# and store into not_null_twitter
-# for example if row 0 exist associate twitter handle,
-# then it will be stored in not_null_twitter
-not_null_twitter = df.loc[pd.notna(df["Associated Twitter Handle"]), :].index
-
-# create new rows for every Associate Twitter Handle,
-# in this case source will be the old assciated twitter handle
-# Type to be "Twitter Handle", UniqueID, Tags and Name will keep same.
-new_df_to_add_twitter = pd.DataFrame({'UniqueID': df.iloc[not_null_twitter, 0],  # nopep8
-                              'Source': df.iloc[not_null_twitter, 5],  # nopep8
-                              "RSS feed URLs (where available)": np.nan,  # nopep8
-                              'Type': "Twitter Handle",  # nopep8
-                              'Tags': df.iloc[not_null_twitter, 4],  # nopep8
-                              'Associated Twitter Handle': np.nan,
-                              'Associated Publisher': np.nan,  # nopep8
-                              'Name': df.iloc[not_null_twitter, 7],  # nopep8
-                              'Text aliases': np.nan})
-
-# since some of associated twitter Handle will be 2 or 3,
-# so it use "|" to split
-# and make every twitter handle as a new record
-new_df_to_add_twitter["Source"] = new_df_to_add_twitter["Source"].str.split("|")  # nopep8
-new_df_to_add_twitter = new_df_to_add_twitter.explode("Source")
-new_df_to_add_twitter
+    # since some of associated twitter Handle will be 2 or 3,
+    # so it use "|" to split
+    # and make every twitter handle as a new record
+    new_df_to_add_twitter["Source"] = new_df_to_add_twitter["Source"].str.split("|")  # nopep8
+    new_df_to_add_twitter = new_df_to_add_twitter.explode("Source")
+    return new_df_to_add_twitter
 
 
-# In[4]:
+def separate_by_type(df):
+    """
+    separate the dataframe that group by Type
+    :param df: a dataframe
+    :rtype: list of dataframe
+    """
+
+    Object = [x for _, x in df.groupby('Type')]
+    return Object
 
 
-# add the new rows into the origin dataframe and sorted by UniqueID
-df = df.append(new_df_to_add_twitter)
-df = df.sort_values(by=['UniqueID'])
-df = df.reset_index()
-df.drop(['index'], axis=1, inplace=True)
-df
+def get_twitter_csv(file1):
+    """
+    generate twitter.csv with unicode version
+    :param df: a dataframe
+    :rtype: None
+    """
+    file1.to_csv('twitter.csv', index=False, encoding='utf-8-sig')
 
 
-# In[5]:
-
-
-Object = [x for _, x in df.groupby('Type')]
-Object[0]
+def get_domain_csv(file0):
+    """
+    generate domain.csv with unicode version
+    :param df: a dataframe
+    :rtype: None
+    """
+    file0.to_csv('domain.csv', index=False, encoding='utf-8-sig')
 
 
 # In[6]:
 
 
-Object[1]
+def scope_parser(file):
+    """
+    do the whole scoper_parser's process
+    :param file: a csv file
+    :rtype: list of dataframe
+    """
+    # read the csv file
+    df = pd.read_csv(file)
+
+    # create a new column named "UniqueID" from 1,2,3,4,5,
+    # ...to n(n is # of rows we have)
+    # UniqueID is using to link the Source and Associate Twitter Handle,
+    # which should create by system
+    # usually it is used by system only
+    insert_UniqueID(df)
+
+    expand_records = expand_Twitter_Handle(df)
+    # add expand_records into dafaframe and reset the index to 0,1,2,3....
+    df = df.append(expand_records)
+    df = df.sort_values(by=['UniqueID'])
+    df = df.reset_index()
+    df.drop(['index'], axis=1, inplace=True)
+
+    # separate dataframe that group by Type
+    Object = separate_by_type(df)
+    # generate twitter csv
+    print("generate twitter.csv")
+    get_twitter_csv(Object[1])
+    # generate domain csv
+    print("generate domain.csv")
+    get_domain_csv(Object[0])
+
+    print("Congratulations, you could move to next step!")
+    # returen dataframe
+    return df
 
 
-# In[7]:
-
-
-print('l1')
-print(Object[1].iloc[0, ])
-print('l2')
-print(Object[1].iloc[1, ])
-print('l3')
-print(Object[1].iloc[2, ])
-print('l4')
-print(Object[1].iloc[3, ])
-
-
-# In[8]:
-
-
-def get_twitter_csv(file1):
-    file1.to_csv('twitter.csv', index=False)
-
-
-def get_domain_csv(file0):
-    file0.to_csv('domain.csv', index=False)
-
-
-get_twitter_csv(Object[1])
-get_twitter_csv(Object[0])
+if __name__ == '__main__':
+    inFile = sys.argv[1]
+    dataframes = scope_parser("test_demo.csv")
---
+++ scope_parser/validate.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+import numpy as np
+import urllib.request
+import sys
+import chardet
+
+import scope_parser as sp
+
+
+# check the input file should be csv file
+def is_csv(filename):
+    """
+    check input file is csv or not, if it is, change it to unicode
+    :param filename: a csv file name
+    :rtype: Boolean if it is correct, exit process if it isn't csv
+    """
+    try:
+        # try to read csv file
+        df = pd.read_csv(filename)
+        # restore it as unicode
+        df.to_csv(filename, index=False, encoding='utf-8-sig')
+        return True
+    except(Exception):
+        print("Input file is not csv or doesn't exist such csv")
+        exit(1)
+
+
+def check_unicode(filename):
+    """
+    check the csv file is unicode or not
+    :param filename: a csv file name
+    :rtype: None
+    """
+    with open(filename, 'rb') as detect_file_encoding:
+        detection = chardet.detect(detect_file_encoding.read())
+        print('Chardet:', detection)
+
+
+# In[4]:
+
+
+def check_ATH(df):
+    """
+    find every rows that all Associated Twitter Handle
+    is incorrect and print the row number
+    :param df: a dataframe
+    :rtype: data frame
+    """
+    return(df.loc[~(df["Associated Twitter Handle"].isnull() | df["Associated Twitter Handle"].str.startswith('@'))])  # nopep8
+
+
+# In[5]:
+
+
+def url_is_alive(url):
+    """
+    Checks that a given URL is reachable.
+    :param url: A URL
+    :rtype: bool
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except(Exception):
+        return False
+
+
+def check_domain(df):
+    """
+    Checks that a given df's url is reachable and return invalid dataframe.
+    :param df: A dataframe
+    :rtype: a dataframe
+    """
+
+    domain = df.loc[df["Type"] == "News Source"].copy(deep=True)
+    result = domain["Source"].apply(url_is_alive)
+    domain.loc[:, 'domain_valid'] = result
+    domain = domain.loc[domain["domain_valid"] == False]  # nopep8
+    del domain['domain_valid']
+    return domain
+
+
+def check_ATH(df):
+    """
+    Checks that all Associated Twitter Handle is valid or not
+    and return invalid dataframe.
+    :param df: A dataframe
+    :rtype: a dataframe
+    """
+    return(df.loc[~(df["Associated Twitter Handle"].isnull() | df["Associated Twitter Handle"].str.startswith('@'))])   # nopep8
+
+
+def check_Type(df):
+    """
+    find every rows that Type is not "News Source" or "Twitter Handle"
+    and return invalid rows as a new dataframe.
+    :param df: A dataframe
+    :rtype: a dataframe
+    """
+    new = df.loc[~((df["Type"] == "Twitter Handle") | (df["Type"] == "News Source"))]  # nopep8
+    return(new)
+
+
+# find the source is incorrect format and return index
+def check_Source(df):
+    """
+    Checks that all source(Twitter Handle part) is valid or not
+    and return invalid rows as a new dataframe.
+    :param df: A dataframe
+    :rtype: a dataframe
+    """
+    new = df.loc[df["Type"] == "Twitter Handle"]
+    return(new.loc[~(df["Source"].str.startswith('@'))])
+
+
+def insert_error_type(error_name):
+    """
+    Create a new dataframe stand for error name
+    :param error_name: A string of error name
+    :rtype: A dataframe
+    """
+
+    df = pd.DataFrame({'Source': [error_name],
+                       "RSS feed URLs (where available)": np.nan,
+                       'Type': np.nan,
+                       'Tags': np.nan,
+                       'Associated Twitter Handle': np.nan,
+                       'Associated Publisher': np.nan,
+                       'Name': np.nan,
+                       'Text aliases': np.nan})
+    return(df)
+
+
+def validation(file):
+    """
+    validate file is valid or not,
+    if it is valid, generate twitter.csv and domain.csv
+    if it is invalid, print error type and size and generate error.csv
+    :param df: A dataframe
+    :rtype: None
+    """
+    # check whether the file has error or not
+    error_key = 0
+    # test does it is csv file and convert file into unicode
+    is_csv(file)
+    print("It is valid csv file")
+    # read the file as data frame
+    df = pd.read_csv(file)
+    # create a new_dataframe for error records
+    Error = pd.DataFrame(columns=['Source', 'RSS feed URLs (where available)', 'Type', 'Tags', 'Associated Twitter Handle', 'Associated Publisher', 'Name', 'Text aliases'])  # nopep8
+
+    print("check Type")
+    error_type = check_Type(df)
+    print("There are %d lines with Type errors" % error_type.index.size)
+    if error_type.index.size != 0:
+        error_key = 1
+        type_error_name = insert_error_type("Type error")
+        Error = Error.append(type_error_name)
+        Error = Error.append(error_type)
+
+    print("check Url")
+    error_Url = check_domain(df)
+    print("There are %d lines with invaild Url errors" % error_Url.index.size)
+    if error_Url.index.size != 0:
+        error_key = 1
+        Url_error_name = insert_error_type("URL error")
+        Error = Error.append(Url_error_name)
+        Error = Error.append(error_Url)
+
+    print("check Associate Twitter Handle")
+    error_ATH = check_ATH(df)
+
+    print("There are %d lines with incorrect Associate Twitter Handle errors" % error_ATH.index.size)  # nopep8
+    if error_ATH.index.size != 0:
+        error_key = 1
+        ATH_error_name = insert_error_type("Associate Twitter Handle error")
+        Error = Error.append(ATH_error_name)
+        Error = Error.append(error_ATH)
+
+    print("check Twitter Handle")
+    error_TH = check_Source(df)
+    print("There are %d lines with incorrect Twitter Handle errors" % error_TH.index.size)  # nopep8
+    if error_TH.index.size != 0:
+        error_key = 1
+        TH_error_name = insert_error_type("Twitter Handle error")
+        Error = Error.append(TH_error_name)
+        Error = Error.append(error_TH)
+
+    # if it has error, generate error.csv
+    if error_key == 1:
+        print("csv exists some errors, please fix it")
+        print("generate error.csv")
+        Error.to_csv('error.csv', index=True, encoding='utf-8-sig')
+        return None
+    else:
+        sp.scope_parser(file)
+        print("Valid")
+
+
+if __name__ == '__main__':
+
+    inFile = sys.argv[1]
+    validation(inFile)
