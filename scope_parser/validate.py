9d7eabce778b55bfd2b0bd5d791350793df2a1fe
Jacqueline <jacquelinechan2@hotmail.com>
2020-11-17 16:39:49 -0500

    try catch urllib to find the 403 error and flush the output so that output can reach node scripts' output

--- scope_parser/validate.py
+++ scope_parser/validate.py
@@ -8,6 +8,8 @@ import pandas as pd
 import numpy as np
 import urllib.request
 import sys
+import traceback 
+
 import chardet
 
 import scope_parser as sp
@@ -35,6 +37,7 @@ def is_csv(filename):
               'Input file is invalid.' +
               ' It is not a csv file or it is corrupted.' +
               Style.RESET_ALL)
+        sys.stdout.flush()
         raise
 
 
@@ -47,6 +50,7 @@ def check_unicode(filename):
     with open(filename, 'rb') as detect_file_encoding:
         detection = chardet.detect(detect_file_encoding.read())
         print('Chardet:', detection)
+        sys.stdout.flush()
 
 
 def url_is_alive(url):
@@ -55,13 +59,17 @@ def url_is_alive(url):
     :param url: A URL
     :rtype: bool
     """
-    request = urllib.request.Request(url)
-    request.get_method = lambda: 'HEAD'
+    print('Currently checking url: ' + url)
+    sys.stdout.flush()
 
     try:
-        urllib.request.urlopen(request)
+        request = urllib.request.Request(url)
+        request.get_method = lambda: 'HEAD'
+        urllib.request.urlopen(request, timeout=300)
         return True
     except(Exception):
+        print(traceback.print_exc())
+        sys.stdout.flush()
         return False
 
 
@@ -142,17 +150,21 @@ def validation(file):
     # check whether the file has error or not
     error_key = 0
     print(Fore.BLUE + "checking CSV:" + Style.RESET_ALL)
+    sys.stdout.flush()
     # test does it is csv file and convert file into unicode
     is_csv(file)
     print("It is a valid csv file")
+    sys.stdout.flush()
     # read the file as data frame
     df = pd.read_csv(file)
     # create a new_dataframe for error records
     Error = pd.DataFrame(columns=['Source', 'RSS feed URLs (where available)', 'Type', 'Tags', 'Associated Twitter Handle', 'Associated Publisher', 'Name', 'Text aliases'])  # nopep8
 
     print(Fore.BLUE + "checking Type:" + Style.RESET_ALL)
+    sys.stdout.flush()
     error_type = check_Type(df)
     print("There are %d lines with Type errors" % error_type.index.size)
+    sys.stdout.flush()
     if error_type.index.size != 0:
         error_key = 1
         type_error_name = insert_error_type("Type error")
@@ -160,8 +172,10 @@ def validation(file):
         Error = Error.append(error_type)
 
     print(Fore.BLUE + "checking URL:" + Style.RESET_ALL)
+    sys.stdout.flush()
     error_Url = check_domain(df)
     print("There are %d lines with invaild Url errors" % error_Url.index.size)
+    sys.stdout.flush()
     if error_Url.index.size != 0:
         error_key = 1
         Url_error_name = insert_error_type("URL error")
@@ -169,9 +183,11 @@ def validation(file):
         Error = Error.append(error_Url)
 
     print(Fore.BLUE + "checking Associate Twitter Handle:" + Style.RESET_ALL)
+    sys.stdout.flush()
     error_ATH = check_ATH(df)
 
     print("There are %d lines with incorrect Associate Twitter Handle errors" % error_ATH.index.size)  # nopep8
+    sys.stdout.flush()
     if error_ATH.index.size != 0:
         error_key = 1
         ATH_error_name = insert_error_type("Associate Twitter Handle error")
@@ -179,8 +195,10 @@ def validation(file):
         Error = Error.append(error_ATH)
 
     print(Fore.BLUE + "checking Twitter Handles:" + Style.RESET_ALL)
+    sys.stdout.flush()
     error_TH = check_Source(df)
     print("There are %d lines with incorrect Twitter Handle errors" % error_TH.index.size)  # nopep8
+    sys.stdout.flush()
     if error_TH.index.size != 0:
         error_key = 1
         TH_error_name = insert_error_type("Twitter Handle error")
@@ -190,6 +208,7 @@ def validation(file):
     # if it has error, generate error.csv
     if error_key == 1:
         print(Fore.RED + "Generating error.csv..." + Style.RESET_ALL)
+        sys.stdout.flush()
         Error.to_csv('error.csv', index=True, encoding='utf-8-sig')
         raise Exception(Fore.RED +
               "There exists some csv formating errors," +
@@ -199,6 +218,7 @@ def validation(file):
         sp.scope_parser(file)
         print(Fore.GREEN +
               "Finished Validating and Scope Parsing." + Style.RESET_ALL)
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
