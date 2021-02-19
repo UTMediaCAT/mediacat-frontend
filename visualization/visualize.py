da18015ed830fda8d763eb444620b237b08264fb
AlAndr04 <aandros04@gmail.com>
2021-02-19 16:38:06 -0500

    Fixed travis CI

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -6,8 +6,8 @@ with open("output.json", "r") as output:
     column_set = False
     curr_col = ""
     multi_columns = []
-    entry_id=""
-    with open('output.csv','w', encoding="utf-8") as file:
+    entry_id = ""
+    with open('output.csv', 'w', encoding="utf-8") as file:
         for prefix, event, value in parser:
             # Adds columns
             if event == "map_key":
@@ -16,26 +16,28 @@ with open("output.json", "r") as output:
                 else:
                     curr_col = value
                     if not column_set:
-                        columns+=curr_col+","
-            if prefix == entry_id+"."+curr_col:
-                # Parses [, means there will be an array, so an empty string is created
+                        columns += curr_col + ","
+            if prefix == entry_id+"." + curr_col:
+                # Parses [, means there will be an array,
+                # so an empty string is created
                 if event == "start_array":
                     multi_columns.append("")
-                # Parses ], means the array is done, so the last element of the list
-                # is added to current row
+                # Parses ], means the array is done, so the last element
+                # of the list is added to current row
                 elif event == "end_array":
-                    current+=multi_columns[-1]+","
+                    current += multi_columns[-1] + ","
                 else:
                     if not value:
-                        current +="null,"
+                        current += "null,"
                     elif type(value) == int:
-                        current +=str(value)+","
+                        current += str(value) + ","
                     else:
                         # Replaces commas to not mess up the csv structure
-                        current+=value.replace(",","")+","
-            # This means it's an item of a list, so we add it to the last element of the list
+                        current += value.replace(",","") + ","
+            # This means it's an item of a list, so we add it to the last
+            # element of the list
             elif prefix == entry_id+"."+curr_col+".item":
-                multi_columns[-1]+=value+" "
+                multi_columns[-1] += value + " "
             # Marks the end of the element
             elif entry_id != "" and prefix == entry_id and event == "end_map":
                 if not column_set:
